ef084bcb930f71a02a9d64104abf1f4143c82503
Utkarsh Rao Viswavasu <hootkarsh@gmail.com>
2017-10-24 16:59:59 -0600

    camera class updated

--- CookingSchool/Assets/_Scripts/cameraMovement.cs
+++ CookingSchool/Assets/_Scripts/cameraMovement.cs
@@ -4,20 +4,32 @@ using UnityEngine;
 
 public class cameraMovement : MonoBehaviour {
 
-	// Use this for initialization
+    // Use this for initialization
+    private Vector3[] cameraPositions = new Vector3[8];
 	void Start () {
-        transform.position = new Vector3(-7.5f, 1.65f, 6.75f);
-        
+        cameraPositions[0] = new Vector3(-7.5f, 1.65f, 6.75f);
+        cameraPositions[1] = new Vector3(-5.51f, 1.65f, 1.72f);
+        Vector3 temp = cameraPositions[1];
+        for (int i=2;i<8;i++)
+        {
+            temp.x -= 0.85f;
+            cameraPositions[i] = temp;
+        }
     }
 	
 	// Update is called once per frame
 	void Update () {
-        //smoothMove(new Vector3(-4.51f, 1.65f, 1.72f));
+       
+            smoothMove(4);
+            Debug.Log("key pressed");
+        
     }
 
-    void smoothMove(Vector3 targetPos)
-    { 
+    void smoothMove(int index)
+    {
+        
+        Debug.Log("Moving to position " + index);
+        this.transform.position = Vector3.Lerp(transform.position, cameraPositions[index], 1.4f*Time.deltaTime);
 
-        transform.position = Vector3.Lerp(transform.position, targetPos, 1.5f * Time.deltaTime);
     }
 }
