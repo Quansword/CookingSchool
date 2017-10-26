f7020126c7c7192f3579d7f0020a4a88c58a8fec
Quansword <Quanbook@gmail.com>
2017-10-26 02:57:05 -0600

    Added more classes

---
+++ CookingSchool/Assets/_Scripts/Items/Containers.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Containers : Items
+{
+	public bool inUse;
+	public bool mixed;
+
+	public List<Ingredients> itemList = new List<Ingredients>();
+
+	public Containers()
+	{
+		inUse = false;
+		mixed = false;
+		Debug.Log("Default Container Constructor Called");
+	}
+
+	public Containers(Type iType, Location iLocation)
+	{
+		itemType = iType;
+		itemLocation = iLocation;
+		inUse = false;
+		mixed = false;
+		Debug.Log("Container Constructor Called");
+	}
+}
---
+++ CookingSchool/Assets/_Scripts/Items/Tools.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tools : Items
+{
+	public bool inUse;
+
+	public List<Ingredients> itemList = new List<Ingredients>();
+
+	public Tools()
+	{
+		inUse = false;
+		Debug.Log("Default Container Constructor Called");
+	}
+
+	public Tools(Type iType, Location iLocation)
+	{
+		itemType = iType;
+		itemLocation = iLocation;
+		inUse = false;
+		Debug.Log("Container Constructor Called");
+	}
+}
