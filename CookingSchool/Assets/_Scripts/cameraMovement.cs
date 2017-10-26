1c6fff22227f397b23cbe5de58aedb8cba2bc9fa
Utkarsh Rao Viswavasu <hootkarsh@gmail.com>
2017-10-26 11:02:57 -0600

    camera zones working with mouse input

--- CookingSchool/Assets/_Scripts/cameraMovement.cs
+++ CookingSchool/Assets/_Scripts/cameraMovement.cs
@@ -21,9 +21,38 @@ public class cameraMovement : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-        if (Input.anyKeyDown)
+        if (Input.GetMouseButtonUp(0))
         {
-            moveCoroutine = smoothMove(3);
+            int posIndex=0;
+            if (Input.mousePosition.x < Screen.width / 8 * 1)
+            {
+                posIndex = 1;
+            }
+            else if (Input.mousePosition.x < Screen.width / 8 * 2)
+            {
+                posIndex = 2;
+            }
+            else if (Input.mousePosition.x < Screen.width / 8 * 3)
+            {
+                posIndex = 3;
+            }
+            else if (Input.mousePosition.x < Screen.width / 8 * 4)
+            {
+                posIndex = 4;
+            }
+            else if (Input.mousePosition.x < Screen.width / 8 * 5)
+            {
+                posIndex = 5;
+            }
+            else if (Input.mousePosition.x < Screen.width / 8 * 6)
+            {
+                posIndex = 6;
+            }
+            else if (Input.mousePosition.x < Screen.width / 8 * 7)
+            {
+                posIndex = 7;
+            }
+            moveCoroutine = smoothMove(posIndex);
             StartCoroutine(moveCoroutine);
             Debug.Log("Pressed a key");
         }
@@ -36,9 +65,9 @@ public class cameraMovement : MonoBehaviour {
         Debug.Log("Moving to position " + index);
         while (Time.time < startTime + 2f)
         {
-            transform.position = Vector3.Lerp(transform.position, cameraPositions[index], 1.4f * Time.deltaTime);
+            transform.position = Vector3.Lerp(transform.position, cameraPositions[index], 1.0f * Time.deltaTime);
             yield return null;
         }
-        transform.position = cameraPositions[index];
+        
     }
 }
