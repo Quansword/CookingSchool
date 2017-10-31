7d93834b7c35f599118fd52b1630c71a35b59d5b
Utkarsh Rao Viswavasu <hootkarsh@gmail.com>
2017-10-30 20:48:29 -0600

    camera pretty much fully working, movement is slightly jerky though

--- CookingSchool/Assets/_Scripts/cameraMovement.cs
+++ CookingSchool/Assets/_Scripts/cameraMovement.cs
@@ -32,53 +32,73 @@ public class cameraMovement : MonoBehaviour {
         cameraRotations[1] = new Quaternion(0, 180, -17.693f, 1);
         cameraPositions[2] = new Vector3(-5.5f, 1.82f, 1.84f);
         cameraRotations[2] = new Quaternion(0, 180, -24.839f, 0);
-        Vector3 temp = cameraPositions[1];
+        Vector3 temp = cameraPositions[2];
         for (int i=3;i<8;i++)
         {
             temp.x -= 1.0f;
             cameraPositions[i] = temp;
             cameraRotations[i] = cameraRotations[2];
         }
+        camLocation = 0;
     }
 	
 	// Update is called once per frame
 	void Update () {
 
-        if (Input.GetMouseButtonUp(0))
+        if (Input.GetMouseButtonUp(0) && camLocation == 0)
         {
-            int posIndex=0;
-            if (Input.mousePosition.x < Screen.width / 8 * 1)
+            if (Input.mousePosition.x < Screen.width / 7 * 1)
             {
-                posIndex = 1;
+                camLocation = 1;
             }
-            else if (Input.mousePosition.x < Screen.width / 8 * 2)
+            else if (Input.mousePosition.x < Screen.width / 7 * 2)
             {
-                posIndex = 2;
+                camLocation = 2;
             }
-            else if (Input.mousePosition.x < Screen.width / 8 * 3)
+            else if (Input.mousePosition.x < Screen.width / 7 * 3)
             {
-                posIndex = 3;
+                camLocation = 3;
             }
-            else if (Input.mousePosition.x < Screen.width / 8 * 4)
+            else if (Input.mousePosition.x < Screen.width / 7 * 4)
             {
-                posIndex = 4;
+                camLocation = 4;
             }
-            else if (Input.mousePosition.x < Screen.width / 8 * 5)
+            else if (Input.mousePosition.x < Screen.width / 7 * 5)
             {
-                posIndex = 5;
+                camLocation = 5;
             }
-            else if (Input.mousePosition.x < Screen.width / 8 * 6)
+            else if (Input.mousePosition.x < Screen.width / 7 * 6)
             {
-                posIndex = 6;
+                camLocation = 6;
             }
-            else if (Input.mousePosition.x < Screen.width / 8 * 7)
+            else if (Input.mousePosition.x < Screen.width / 7 * 7)
             {
-                posIndex = 7;
+                camLocation = 7;
             }
-            moveCoroutine = smoothMove(posIndex);
+            moveCoroutine = smoothMove(camLocation);
             StartCoroutine(moveCoroutine);
-            transform.rotation = cameraRotations[posIndex];
-            Debug.Log("Pressed a key");
+            transform.rotation = cameraRotations[camLocation];
+            Debug.Log("Pressed a key in main screen");
+        }
+        else if (Input.GetMouseButtonUp(0) && camLocation > 0 )
+        {
+            if (Input.mousePosition.x < Screen.width / 7)
+            {
+                camLocation--;
+            }
+            else if (Input.mousePosition.x < Screen.width && Input.mousePosition.x > Screen.width/7*6 && camLocation < 7)
+            {
+                camLocation++;
+            }
+            moveCoroutine = smoothMove(camLocation);
+            StartCoroutine(moveCoroutine);
+        }
+        else if (Input.GetKeyUp(KeyCode.Escape) && camLocation>0)
+        {
+            camLocation = 0;
+            moveCoroutine = smoothMove(camLocation);
+            StartCoroutine(moveCoroutine);
+            transform.rotation = cameraRotations[camLocation];
         }
         
     }
@@ -89,7 +109,7 @@ public class cameraMovement : MonoBehaviour {
         Debug.Log("Moving to position " + index);
         while (Time.time < startTime + 2f)
         {
-            transform.position = Vector3.Lerp(transform.position, cameraPositions[index], 1.0f * Time.deltaTime);
+            transform.position = Vector3.Lerp(transform.position, cameraPositions[index], 2.0f * Time.deltaTime);
             yield return null;
         }
         transform.position = cameraPositions[index];
