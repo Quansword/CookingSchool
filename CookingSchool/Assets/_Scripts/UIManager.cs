ec5755a238775e58dcd7e4923463abd880c866a8
Utkarsh Rao Viswavasu <hootkarsh@gmail.com>
2017-11-02 03:28:19 -0600

    title screen, stove etc

--- CookingSchool/Assets/_Scripts/UIManager.cs
+++ CookingSchool/Assets/_Scripts/UIManager.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Recipe : MonoBehaviour
+public class UIManager : MonoBehaviour
 {
 
 
@@ -11,7 +11,8 @@ public class Recipe : MonoBehaviour
     public Camera mainCamera;
     private cameraMovement cam;
     public Text screenText;
-    public GameObject[] objects = new GameObject[10];
+    public GameObject[] objects = new GameObject[11];
+    private bool flameOn;
 
     private int index = 0;
 
@@ -22,9 +23,9 @@ public class Recipe : MonoBehaviour
         recipeText.Add("Cooking School");
         recipeText.Add("Ingredients:\n 4 eggs \n 1/4 cup whole milk \n 1 tablespoon butter \n salt and pepper(to taste) ");
 
-        recipeText.Add("Crack eggs into mixing bowl and beat them until the mixture is a uniform a pale yellow ");
+        recipeText.Add("Crack eggs into mixing bowl ");
         recipeText.Add("Add Milk, Salt, and Pepper to the egg mixture");
-        recipeText.Add("Whisk egg mixture thoroughly");
+        recipeText.Add("Whisk egg mixture thoroughly\n\n Whisk: to mix a liquid quickly with a fork to make the eggs fluffy");
         recipeText.Add("Heat a non-stick pan over a low medium heat");
         recipeText.Add("Test the heat");
         recipeText.Add("Melt butter onto the pan");
@@ -32,7 +33,7 @@ public class Recipe : MonoBehaviour
         recipeText.Add("Stir gently until all the liquid is gone");
         recipeText.Add("Turn off the heat");
         recipeText.Add("Transfer eggs onto a plate");
-
+        flameOn = false;
 
     }
 
@@ -41,12 +42,13 @@ public class Recipe : MonoBehaviour
     {
         if(cam.camLocation == 0)
         {
-            for (int i = 0; i < 5; i++)
+            for (int i = 0; i < 11; i++)
             {
                 objects[i].SetActive(false);
 
             }
         }
+        
         else
         {
             for (int i = 0; i < 5; i++)
@@ -55,6 +57,10 @@ public class Recipe : MonoBehaviour
 
             }
         }
+        if (cam.camLocation == 6 && !flameOn)
+        {
+            objects[5].SetActive(true);
+        }
         if (index == 12)
         {
             index = 0;
@@ -62,10 +68,32 @@ public class Recipe : MonoBehaviour
     }
     public void nextStep()
     {
-        screenText.text = recipeText[++index];
+        ++index;
+        if (index > 11) { index = 11; }
+        screenText.text = recipeText[index];
     }
     public void prevStep()
     {
-        screenText.text = recipeText[--index];
+        --index;
+        if (index < 0) { index = 0; }
+        screenText.text = recipeText[index];
+    }
+
+    public void turnOnStove()
+    {
+        objects[5].SetActive(false);
+        flameOn = true;
+        for (int i = 6;i<10;i++)
+        {
+            objects[i].SetActive(true);
+        }
+    }
+    public void selectOption()
+    {
+        for (int i = 6; i < 10; i++)
+        {
+            objects[i].SetActive(false);
+        }
+        objects[10].SetActive(true);
     }
 }
